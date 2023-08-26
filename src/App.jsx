a85990490af9d4ce6de7f7c68263459b6830f21a
tylersb <tylersb@gmail.com>
2023-08-25 18:54:43 -0700

    adds component to add todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Todo from './components/Todo'
+import TodoForm from './components/TodoForm'
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -9,15 +10,19 @@ export default function App() {
     { text: 'Build really cool todo app' }
   ])
 
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text }]
+    setTodos(newTodos)
+  }
+
   return (
-    <>
-      <div className="app">
-        <div className="todo-list">
-          {todos.map((todo, index) => (
-            <Todo key={index} index={index} todo={todo} />
-          ))}
-        </div>
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
+        <TodoForm addTodo={addTodo} />
       </div>
-    </>
+    </div>
   )
 }
